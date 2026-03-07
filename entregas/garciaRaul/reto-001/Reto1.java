f49f74c73a2fcd2e360942af237d172038513aee
Raúl García Roldán <144956120+xImRaul@users.noreply.github.com>
2026-03-07 03:39:08 +0100

    Reorganizacion del formato del codigo

--- entregas/garciaRaul/reto-001/Reto1.java
+++ entregas/garciaRaul/reto-001/Reto1.java
@@ -1,36 +1,35 @@
 class Reto1 {
     public static void main(String[] args) {
-
-        int billetes100 = 0;
-        int billetes50 = 0;
-        int billetes20 = 0;
-        int billetes10 = 0;
-        int billetes5 = 0;
-        int monedas2 = 0;
-        int monedas1 = 0;
-
+        
         int cantidad_Pagar = 400;
         int cantidad_Entregada = 15532;
-
+        
         System.out.println("Debes pagar: " + cantidad_Pagar);
         System.out.println("Has entregado: " + cantidad_Entregada);
         System.out.println("-------------------------");
 
+        
         int cantidad_Devolver = cantidad_Entregada - cantidad_Pagar;
 
-        billetes100 = cantidad_Devolver / 100;
+        int billetes100 = cantidad_Devolver / 100;
         cantidad_Devolver = cantidad_Devolver - (100 * billetes100);
-        billetes50 = cantidad_Devolver / 50;
+
+        int billetes50 = cantidad_Devolver / 50;
         cantidad_Devolver = cantidad_Devolver - (50 * billetes50);
-        billetes20 = cantidad_Devolver / 20;
+
+        int billetes20 = cantidad_Devolver / 20;
         cantidad_Devolver = cantidad_Devolver - (20 * billetes20);
-        billetes10 = cantidad_Devolver / 10;
+
+        int billetes10 = cantidad_Devolver / 10;
         cantidad_Devolver = cantidad_Devolver - (10 * billetes10);
-        billetes5 = cantidad_Devolver / 5;
+
+        int billetes5 = cantidad_Devolver / 5;
         cantidad_Devolver = cantidad_Devolver - (5 * billetes5);
-        monedas2 = cantidad_Devolver / 2;
+
+        int monedas2 = cantidad_Devolver / 2;
         cantidad_Devolver = cantidad_Devolver - (2 * monedas2);
-        monedas1 = cantidad_Devolver / 1;
+
+        int monedas1 = cantidad_Devolver / 1;
         cantidad_Devolver = cantidad_Devolver - (1 * monedas1);
 
         System.out.println(billetes100 + " billete(s) de 100€");
@@ -40,6 +39,5 @@ class Reto1 {
         System.out.println(billetes5 + " billete(s) de 5€");
         System.out.println(monedas2 + " moneda(s) de 2€");
         System.out.println(monedas1 + " moneda(s) de 1€");
-
     }
 }
